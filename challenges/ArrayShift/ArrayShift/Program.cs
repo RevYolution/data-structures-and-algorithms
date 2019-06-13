0d416b9b3f9e7501db57755f11ee5f6544837a05
Jon Rice <revyolution1120@gmail.com>
2019-06-13 16:47:58 -0700

    Code working

--- challenges/ArrayShift/ArrayShift/Program.cs
+++ challenges/ArrayShift/ArrayShift/Program.cs
@@ -8,29 +8,28 @@ namespace ArrayShift
         {
             int[] array = {1,2,3,4,5};
             int insertion = 11;
-            Console.WriteLine(",", insertShiftArray(array, insertion));
-            insertShiftArray(array, insertion);
+            Console.WriteLine(String.Join(", ", ShiftArray(array, insertion)));
+            ShiftArray(array, insertion);
         }
-        // Floor half way point. Create new empty array and add in values from previous values with addition of new value. Create if else statement related to midpoint. Add a counter.  
-        public static int[] insertShiftArray(int[] array, int insertion)
+        static int[] ShiftArray(int[] array, int insertion)
         {
-            int mid = array.Length / 2;
-            int[] newArray = new int[array.Length +1];
+            int middle = array.Length / 2;
+            int[] newArray = new int[array.Length + 1];
             int counter = 0;
 
-            for (int i = 0; i <= newArray.Length; i++)
+            for (int i = 0; i < newArray.Length; i++)
             {
-                if (newArray[i] = mid)
+                if (i == middle)
                 {
                     newArray[i] = insertion;
                 }
-                else 
+                else
                 {
-                    newArray[i] = array[counter++];
+                    newArray[i] = array[counter];
+                    counter++;
                 }
-                
-                return newArray;
-             }
+            }
+            return newArray;
         }
     }
 }
