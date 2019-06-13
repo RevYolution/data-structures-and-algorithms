2ffb0b6debec8aac403ae4abdd0dc7ddf62ebacf
Jon Rice <revyolution1120@gmail.com>
2019-06-12 20:45:41 -0700

    Code in for the array shift.

--- challenges/ArrayShift/ArrayShift/Program.cs
+++ challenges/ArrayShift/ArrayShift/Program.cs
@@ -2,11 +2,27 @@
 
 namespace ArrayShift
 {
-    class Program
+   public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            insertShiftArray();
+        }
+
+        public static int[] insertShiftArray()
+        {
+            int[] array = new int[5] { 1, 3, 4, 8, 10 };
+            int insertion = 9;
+            for (int i = 0; i <= array.Length; i++)
+            {
+                if (i >= (array.Length)/2 && i < array.Length+1)
+                {
+                    i = insertion;
+                }
+            }
+                int[] insertArray = array;
+            Console.WriteLine(insertArray);
+                return insertArray;
         }
     }
 }
