c57b80dee7fc38cc74ba519bed0a648e91ec25f4
Jon Rice <revyolution1120@gmail.com>
2019-06-16 14:27:01 -0700

    Code working with comments.

--- challenges/ArrayShift/ArrayShift/Program.cs
+++ challenges/ArrayShift/ArrayShift/Program.cs
@@ -11,6 +11,12 @@ namespace ArrayShift
             Console.WriteLine(String.Join(", ", ShiftArray(array, insertion)));
             ShiftArray(array, insertion);
         }
+        /// <summary>
+        /// Takes in an array and inserts given number at mid point of the array
+        /// </summary>
+        /// <param name="array">Integer array</param>
+        /// <param name="insertion">Integer to be inserted at midpoint</param>
+        /// <returns></returns>
         static int[] ShiftArray(int[] array, int insertion)
         {
             int middle = array.Length / 2;
