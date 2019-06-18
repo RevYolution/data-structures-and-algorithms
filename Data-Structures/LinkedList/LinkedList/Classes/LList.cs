529cab3f4f5149dbc77271e4be360c1f8fdcbf8e
Jon Rice <revyolution1120@gmail.com>
2019-06-18 12:54:53 -0700

    Added list basics.

---
+++ Data-Structures/LinkedList/LinkedList/Classes/LList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList.Classes
+{
+    class LList
+    {
+        public Node Head { get; set; }
+        public LList()
+        {
+
+        }
+
+        public LList(int value)
+        {
+            Node node = new Node(value);
+            Head = node;
+        }
+
+        public void Insert(int value)
+        {
+            Node node = new Node(value);
+            node.Next = Head;
+            Head = node;
+        }
+
+        public void Print()
+        {
+            Node current = Head;
+            while (current != null)
+            {
+                Console.Write($"{current.Value} => ");
+                current = current.Next;
+            }
+            Console.WriteLine("null");
+        }
+        public bool Includes(int value)
+        {
+            Node searchValue = new Node(value);
+            while (searchValue != value)
+            {
+
+            }
+        }
+    }
+
+}
