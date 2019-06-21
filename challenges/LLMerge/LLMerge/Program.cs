d68adae0b37f5dee3d94a412b2abf914c9aeb1b3
Jon Rice <revyolution1120@gmail.com>
2019-06-20 20:39:29 -0700

    Logic for merge list done.

--- challenges/LLMerge/LLMerge/Program.cs
+++ challenges/LLMerge/LLMerge/Program.cs
@@ -9,9 +9,26 @@ namespace LLMerge
             Console.WriteLine("Hello World!");
         }
 
-        public static void MergeLists()
+        public static void MergeLists(LinkList listOne, LinkList listTwo)
         {
+            listOne.Current = listOne.Head;
+            listTwo.Current = listTwo.Head;
 
+            while(listOne.Current != null && listTwo.Current != null)
+            {
+                listTwo.Current = listTwo.Head;
+                listTwo.Current.Next = listOne.Current.Next;
+                listOne.Current.Next = listTwo.Current;
+                listOne.Current = listTwo.Current.Next;
+                listTwo.Current = listTwo.Head;
+            }
+
+            if (listOne.Current.Next == null)
+            {
+                listOne.Current.Next = listTwo.Current;
+            }
+
+            return listOne;
         }
     }
 }
