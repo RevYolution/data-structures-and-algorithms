f8ebacaee22f69a7011ced866f418bb1ad3a3800
Jon Rice <revyolution1120@gmail.com>
2019-06-23 13:51:45 -0700

    Test setup stack class set up. Staged changes too.

---
+++ challenges/LLMerge/LLMerge/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace LLMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void MergeLists(LinkList listOne, LinkList listTwo)
+        {
+            listOne.Current = listOne.Head;
+            listTwo.Current = listTwo.Head;
+
+            while(listOne.Current != null && listTwo.Current != null)
+            {
+                listTwo.Current = listTwo.Head;
+                listTwo.Current.Next = listOne.Current.Next;
+                listOne.Current.Next = listTwo.Current;
+                listOne.Current = listTwo.Current.Next;
+                listTwo.Current = listTwo.Head;
+            }
+
+            if (listOne.Current.Next == null)
+            {
+                listOne.Current.Next = listTwo.Current;
+            }
+
+            return listOne;
+        }
+    }
+}
