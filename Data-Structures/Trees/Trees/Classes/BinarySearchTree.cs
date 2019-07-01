2e73c1221f1b598448ef90e5791d7df3afc64288
Jon Rice <revyolution1120@gmail.com>
2019-07-01 09:06:47 -0700

    Binary serach tree logic mostly done and test started.

---
+++ Data-Structures/Trees/Trees/Classes/BinarySearchTree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees.Classes
+{
+    class BinarySearchTree
+    {
+        public BinaryTree Tree { get; set; }
+
+        public BinarySearchTree(BinaryTree tree)
+        {
+            Tree = tree;
+        }
+
+        public void Add(Node node)
+        {
+            Node root = Tree.Root;
+
+            if(root == null)
+            {
+                Tree.Root = node;
+            }
+            else
+            {
+                Node last = root;
+                
+                while(root != null)
+                {
+                    last = root;
+
+                    if((int) node.Value > (int) root.Value)
+                    {
+                        root = root.Right;
+                    }
+                    else if((int) node.Value < (int)root.Value)
+                    {
+                        root = root.Left;
+                    }
+
+                }
+                if((int) node.Value > (int)last.Value)
+                {
+                    last.Right = node;
+                }
+                else
+                {
+                    last.Left = node;
+                }
+            }
+        }
+    }
+}
