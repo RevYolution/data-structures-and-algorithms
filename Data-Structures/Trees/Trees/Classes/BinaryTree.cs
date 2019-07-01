d6503e1448e30281db443b920453ea5eb14ae03e
Jon Rice <revyolution1120@gmail.com>
2019-07-01 08:47:59 -0700

    Logic for binary tree search done.

---
+++ Data-Structures/Trees/Trees/Classes/BinaryTree.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees.Classes
+{
+    class BinaryTree
+    {
+        public Node Root { get; set; }
+
+        public BinaryTree(Node root)
+        {
+            Root = root;
+        }
+
+        public static List<Node> PreOrder(Node root, List<Node> nodeArray = null)
+        {
+            if(root == null)
+            {
+                return null;
+            }
+            if(nodeArray == null)
+            {
+                return new List<Node>();
+            }
+
+            nodeArray.Add(root);
+
+            if(root.Left != null) 
+            {
+                PreOrder(root.Left, nodeArray);
+            }
+            if(root.Right != null)
+            {
+                PreOrder(root.Right, nodeArray);
+            }
+
+            return nodeArray;
+        }
+
+        public static List<Node> InOrder(Node root, List<Node> nodeArray = null)
+        {
+            if(root == null)
+            {
+                return null;
+            }
+
+            if(nodeArray == null)
+            {
+                nodeArray = new List<Node>();
+            }
+
+            if(root.Left != null)
+            {
+                InOrder(root.Left, nodeArray);
+            }
+
+            nodeArray.Add(root);
+
+            if(root.Right != null)
+            {
+                InOrder(root.Right, nodeArray);
+            }
+
+            return nodeArray;
+        }
+
+        public static List<Node> PostOrder(Node root, List<Node> nodeArray = null)
+        {
+            if(root == null)
+            {
+                return null;
+            }
+
+            if(nodeArray == null)
+            {
+                nodeArray = new List<Node>();
+            }
+
+            if(root.Left != null)
+            {
+                PostOrder(root.Left, nodeArray);
+            }
+
+            if(root.Right != null)
+            {
+                PostOrder(root.Right, nodeArray);
+            }
+
+            nodeArray.Add(root);
+            return nodeArray;
+        }
+
+    }
+}
