fcfa9cf291a95c7d546c4f51e414e31ccea8bdaa
Jon Rice <revyolution1120@gmail.com>
2019-08-05 13:13:26 -0700

    Logic for get height in. need tests.

---
+++ challenges/BinaryTreeHeight/BinaryTreeHeight/Classes/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees.Classes
+{
+    class Node
+    {
+        /// <summary>
+        /// Gets/sets left node of a tree
+        /// </summary>
+        public Node Left { get; set; }
+
+        /// <summary>
+        /// Gets/set right node of a tree
+        /// </summary>
+        public Node Right { get; set; }
+
+        /// <summary>
+        /// Gets/sets the value of a tree object
+        /// </summary>
+        public Object Value { get; set; }
+
+        public Node(Object value)
+        {
+            Value = value;
+        }
+    }
+}
