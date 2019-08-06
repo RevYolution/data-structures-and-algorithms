4da8df296c04b1df6b5a884dfcececb5c390cdf0
Jon Rice <revyolution1120@gmail.com>
2019-08-05 19:37:03 -0700

    Tests started

---
+++ challenges/BinaryTreeHeight/TreeHeightTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Trees.Classes;
+
+namespace TreeHeightTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CanGetHeight()
+        {
+            BinaryTree testTree = new BinaryTree();
+        }
+    }
+}
