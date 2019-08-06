d3406d43780d3dbca46d3b6bd75bfe8036ef6a6f
Jon Rice <revyolution1120@gmail.com>
2019-08-05 20:11:24 -0700

    Removal of LL dups logic done.

---
+++ challenges/RemoveDuplicatensLL/RemoveDuplicatensLL/Program.cs
@@ -0,0 +1,43 @@
+﻿using LinkedList.Classes;
+using System;
+using LinkedList;
+using System.Collections.Generic;
+
+namespace RemoveDuplicatensLL
+{
+    class Program
+    {
+        public Node Current { get; set; }
+        public Node Head { get; set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public Node LinkedListRemoveDuplicatens(Node node)
+        {
+            Head = node;
+            Current = Head;
+            Node temp = null;
+            HashSet<Node> dups = new HashSet<Node>();
+
+            while (node != null)
+            {
+                if (!dups.Contains(node))
+                {
+                    dups.Add(node);
+                    Current = node.Next;
+                    temp = Current;
+                }
+                else
+                {
+                    temp.Next = Current.Next;
+                    Current.Next = null;
+                    Current = temp.Next;
+                }
+            }
+            return node;
+        }
+    }
+}
