61dbab33a82459d34c815546be55bf3f644d2efe
Jon Rice <revyolution1120@gmail.com>
2019-08-05 20:21:15 -0700

    Logic for unique char in string

---
+++ challenges/AreCharactersUnique/AreCharactersUnique/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AreCharactersUnique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool AreCharactersUnique(string input)
+        {
+            HashSet<char> check = new HashSet<char>();
+
+            foreach (char item in input)
+            {
+                if (!check.Contains(item))
+                {
+                    check.Add(item);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
